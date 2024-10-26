3e9f024fee2d43b048aabd6d68e4488c0e5a3100
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-10-26 19:15:07 +0900

    it works!

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@ import build from "@hono/vite-build/cloudflare-pages";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
+import { mdx } from "@natsuneko-laboratory/honox-mdx-island";
 
-// mdx
-import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+const jsxImportSource = "hono/jsx";
 
 export default defineConfig({
   plugins: [
     honox({ devServer: { adapter } }),
     mdx({
-      jsxImportSource: "hono/jsx",
+      jsxImportSource,
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
     build(),
